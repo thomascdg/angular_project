24aa7fd8a70b5dfa302d3226906b04349b43b3cd
thomas <codognan.thomas@gmail.com>
2026-09-25 21:43:31 +0200

    ok

---
+++ application_task/src/app/task-item/task-item.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-task-item',
+  styleUrl: './task-item.css',
+  templateUrl: './task-item.html',
+})
+export class TaskItem  {}
---
+++ application_task/src/app/task-list/task-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-task-list',
+  styleUrl: './task-list.css',
+  templateUrl: './task-list.html',
+})
+export class TaskList {}
